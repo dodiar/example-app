3a67a979483b49ca7e2a1cfeeda38b78a82a0dbb
USER\asus1 <dodi.arahmat@gmail.com>
2025-02-21 10:10:24 +0700

    create api register

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['message', 'error'], 404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\NewsController;
+use App\Http\Controllers\Api\RegisterController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::apiResource('news', NewsController::class);
+
+Route::post('/register', RegisterController::class);
